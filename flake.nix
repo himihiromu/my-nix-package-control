0d482efb6dababb77550af685c97167330b0b7c5
himihiromu <himihiromu@icloud.com>
2025-02-03 07:07:35 +0900

    first-commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Nix configuration of ymgyt";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs
+    , home-manager
+    , ...
+    }: {
+      nixosConfigurations = {
+        xps15 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+
+          modules = [
+            ./hosts/xps15
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.ymgyt = import ./home/linux;
+            }
+          ];
+        };
+      };
+    };
+}

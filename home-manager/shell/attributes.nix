f5c145e34ed450697a4dbaffaffc040f49e8cee4
root <root@hermes-agent.local-server>
2026-07-23 01:00:34 +0000

    feat: enable shell integration program overrides

--- home-manager/shell/attributes.nix
+++ home-manager/shell/attributes.nix
@@ -3,8 +3,24 @@
   shellIntegrationPrograms,
   ...
 }:
-{
-  programs = lib.genAttrs shellIntegrationPrograms (_: {
+let
+  generatedPrograms = lib.genAttrs shellIntegrationPrograms (_: {
     enable = true;
   });
+
+  programOverrides = {
+    direnv = {
+      nix-direnv.enable = true;
+    };
+
+    zoxide = {
+      options = [
+        "--cmd"
+        "ls"
+      ];
+    };
+  };
+in
+{
+  programs = lib.recursiveUpdate generatedPrograms programOverrides;
 }

f148d39f23b61e3f5202f72b61cc0890c97709c7
himihiromu <himihiromu@icloud.com>
2026-08-04 14:44:59 +0900

    fix: dotfile解決

--- home-manager/shell/zsh.nix
+++ home-manager/shell/zsh.nix
@@ -5,6 +5,7 @@
 }:
 let
   generatedZshDir = "${config.xdg.configHome}/home-manager/zsh";
+  userZshrc = "${config.home.homeDirectory}/.zshrc";
 in
 {
   programs.zsh = {
@@ -13,5 +14,19 @@ in
     # Home Managerの生成先
     dotDir = generatedZshDir;
 
+    # 既存の、chezmoi等で管理している.zshrcを読み込む
+    initContent = lib.mkOrder 500 ''
+      if [[ -f "${userZshrc}" ]]; then
+        source "${userZshrc}"
+      fi
+    '';
+    zsh-abbr = {
+      enable = true;
+      abbreviations = {
+        la = "ls -a";
+        ll = "ls -l";
+        lal = "ls -al";
+      };
+    };
   };
 }

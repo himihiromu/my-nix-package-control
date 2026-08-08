194502ea9b2ba148ee3f351b14eccb472b22b3be
himihiromu <himihiromu@icloud.com>
2026-08-09 02:24:41 +0900

    refactor: split Waybar configuration

--- home-manager/linux/services.nix
+++ home-manager/linux/services.nix
@@ -26,6 +26,8 @@ let
   '';
 in
 {
+  imports = [ ./waybar.nix ];
+
   home.file = {
     "Pictures/Wallpapers/hyprland-wall0.png".source = "${pkgs.hyprland}/share/hypr/wall0.png";
     "Pictures/Wallpapers/hyprland-wall1.png".source = "${pkgs.hyprland}/share/hypr/wall1.png";
@@ -59,89 +61,6 @@ in
           fit_mode = "cover";
         }
       ];
-    };
-  };
-
-  programs.waybar = {
-    enable = true;
-    settings = [
-      {
-        height = 30;
-        spacing = 4;
-        modules-left = [
-          "hyprland/workspaces"
-          "hyprland/submap"
-          "mpris"
-        ];
-        modules-center = [ "hyprland/window" ];
-        modules-right = [
-          "idle_inhibitor"
-          "wireplumber"
-          "network"
-          "cpu"
-          "memory"
-          "temperature"
-          "hyprland/language"
-          "clock"
-          "tray"
-        ];
-
-        "hyprland/window".max-length = 80;
-        "hyprland/language".format = "{short}";
-        mpris = {
-          format = "{player_icon} {dynamic}";
-          format-paused = "{status_icon} {dynamic}";
-          dynamic-len = 40;
-          player-icons.default = "▶";
-          status-icons.paused = "⏸";
-        };
-        idle_inhibitor = {
-          format = "{icon}";
-          format-icons = {
-            activated = "";
-            deactivated = "";
-          };
-        };
-        wireplumber = {
-          format = "{volume}% {icon}";
-          format-muted = "󰖁";
-          format-icons = [
-            ""
-            ""
-            ""
-          ];
-          on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-          on-click-right = "pavucontrol";
-          on-scroll-up = "wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+";
-          on-scroll-down = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
-        };
-        network = {
-          format-wifi = "{essid} ({signalStrength}%) ";
-          format-ethernet = "{ipaddr}/{cidr} 󰈀";
-          format-disconnected = "Disconnected ⚠";
-          tooltip-format = "{ifname} via {gwaddr}";
-        };
-        cpu = {
-          format = "{usage}% ";
-          tooltip = false;
-        };
-        memory.format = "{}% ";
-        temperature = {
-          hwmon-path-abs = "/sys/devices/pci0000:00/0000:00:18.3/hwmon";
-          input-filename = "temp1_input";
-          critical-threshold = 80;
-          format = "{temperatureC}°C ";
-        };
-        clock = {
-          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-          format-alt = "{:%Y-%m-%d}";
-        };
-        tray.spacing = 10;
-      }
-    ];
-    systemd = {
-      enable = true;
-      target = "graphical-session.target";
     };
   };
 
